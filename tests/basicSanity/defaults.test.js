da3f55b5aa7f416b418e3921b51ec15bc5fb50c8
EugeneGoh <heyeugenegoh@gmail.com>
2023-02-23 23:44:43 -0800

    Add one more test

--- tests/basicSanity/defaults.test.js
+++ tests/basicSanity/defaults.test.js
@@ -4,3 +4,8 @@ test('Get Defaults and make sure it is a non-empty object', () => {
   expect(defaultConfigs).not.toBe(null);
   expect(typeof(defaultConfigs)).toBe(typeof({}));
 });
+
+test('Get expected Defaults', () => {
+  expect(defaultConfigs.colors).not.toBe(null);
+});
+
