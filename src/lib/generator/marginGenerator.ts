5c26e51ca65f24ef54141cef6199899ce8d89378
EugeneGoh <heyeugenegoh@gmail.com>
2023-02-27 22:50:08 -0800

    Add margin generator

--- src/lib/generator/marginGenerator.ts
+++ src/lib/generator/marginGenerator.ts
@@ -1,6 +1,8 @@
 
-const marginGenerator = () => void {
-  
+export default function ({ margins }) {
+  for (var margin in margins) {
+    generateCss(`m-${margin}`)
+  }
 }
 
 module.exports = marginGenerator;
